1eb7eb56dc76bfc75a8219f382e8f25c23b252a3
lars <larskonermann@gmail.com>
2025-11-24 17:58:06 +0100

    added comments + tab opens in window

--- next.js/src/app/Sidebar/SideBarTab.tsx
+++ next.js/src/app/Sidebar/SideBarTab.tsx
@@ -1,8 +1,9 @@
+"use client";
 import Image from "next/image";
 // prettier-ignore
 export default function SideBarTab({ icon, name }: { icon: string, name: string }) {
   return (
-    <button className="flex mb-5 hover:bg-[#556483] rounded-2xl p-2 items-center">
+    <button onClick={()=> window.location.replace(`/${name}`)} className="flex mb-5 hover:bg-[#556483] rounded-2xl p-2 items-center">
       <Image src={icon} alt="error" className="mr-3 invert w-10" />
       <p className="text-2xl ">{name}</p>
     </button>
--- next.js/src/app/Sidebar/sidebar.tsx
+++ next.js/src/app/Sidebar/sidebar.tsx
@@ -7,29 +7,38 @@ import view from "./icons/view-stacked.svg";
 import SideBarTab from "./SideBarTab";
 
 export default function SideBar() {
+  let name = "Bert Bertson";
   return (
-    <div className="bg-gray-800 h-full w-full min-w-fit sm:w-1/1 md:w-1/4 xl:w-1/5 2xl:w-1/10">
-      <div className="h-3/6 text-center pt-5">
-        <div className="text-4xl pb-5">Bert Bertson</div>
-        <div className="flex justify-center">
-          <div className="w-fit">
-            <div className="md:hidden">
-              <SideBarTab icon={view} name="Dashboard" />
+    <>
+      {/* Different sidebar sizes for different screen sizes*/}
+      <div className="bg-gray-800 h-full w-full min-w-fit sm:w-1/1 md:w-1/4 xl:w-1/5 2xl:w-1/10">
+        <div className="h-3/6 text-center pt-5">
+          {/*Name of the logged on user */}
+          <div className="text-4xl pb-5">{name}</div>
+          <div className="flex justify-center">
+            <div className="w-fit">
+              {/* only show Dashboard if device width > 768px */}
+              <div className="md:hidden">
+                <SideBarTab icon={view} name="Dashboard" />
+              </div>
+              {/* Sidebar tabs*/}
+              <SideBarTab icon={calender} name="Overzicht" />
+              <SideBarTab icon={clock} name="Reserveringen" />
+              <SideBarTab icon={account} name="Accounts" />
             </div>
-            <SideBarTab icon={calender} name="Overzicht" />
-            <SideBarTab icon={clock} name="Reserveringen" />
-            <SideBarTab icon={account} name="Accounts" />
+          </div>
+        </div>
+        {/* Put content at the bottom of the sidebar */}
+        <div className=" flex-col content-end h-3/6 text-center pb-5">
+          <div className="flex justify-center content-end">
+            <div className="w-fit">
+              {/* Bottom sidebar tabs */}
+              <SideBarTab icon={settings} name="Instellingen" />
+              <SideBarTab icon={logout} name="Logout" />
+            </div>
           </div>
         </div>
       </div>
-      <div className=" flex-col content-end h-3/6 text-center pb-5">
-        <div className="flex justify-center content-end">
-          <div className="w-fit">
-            <SideBarTab icon={settings} name="Instellingen" />
-            <SideBarTab icon={logout} name="Logout" />
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
