34c78a27cfbc73e9ef70640f1e8278e5a17b1573
Zicohh <zicobacker26@gmail.com>
2025-12-01 22:46:11 +0100

    Align with tailwind v4

--- next.js/src/app/dashboard/reservations/Widgets/NieuweReserveringen.tsx
+++ next.js/src/app/dashboard/reservations/Widgets/NieuweReserveringen.tsx
@@ -1,12 +1,12 @@
 export default function NieuweReserveringen() {
-  return (
-    <div className="bg-[#F0F1E3] text-black w-98/100 mt-5 md:mt-0 mb-5 md:mb-0">
-      <div className="h-full relative text-4xl p-10">
-        <div className="h-2/4">Nieuwe reserveringen</div>
-        <div>
-          <div className="text-9xl ">1</div>
+    return (
+        <div className="bg-[#F0F1E3] text-black w-98/100 mt-5 md:mt-0 mb-5 md:mb-0">
+            <div className="h-full relative text-4xl p-8">
+                <div className="h-2/4">Nieuwe reserveringen</div>
+                <div>
+                    <div className="text-9xl ">1</div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
