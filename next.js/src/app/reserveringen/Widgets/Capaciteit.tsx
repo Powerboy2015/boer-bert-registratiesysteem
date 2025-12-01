9c9cafb7d1d4f8e068b179e2ffef87ecf0d6a726
lars <larskonermann@gmail.com>
2025-12-01 10:38:42 +0100

    color changes

--- next.js/src/app/reserveringen/Widgets/Capaciteit.tsx
+++ next.js/src/app/reserveringen/Widgets/Capaciteit.tsx
@@ -1,6 +1,6 @@
 export default function Capaciteit() {
   return (
-    <div className="bg-[#556483] text-black w-98/100 mb-5 md:mb-0">
+    <div className="bg-[#556483] text-white w-98/100 mb-5 md:mb-0">
       <div className=" relative text-4xl p-10">
         <div className="h-2/4">
           <div>Camping capaciteit</div>
