e94df17c23374da2b3355940e3ec3b0d33a221c0
lars <larskonermann@gmail.com>
2025-12-01 09:36:12 +0100

    https://github.com/Powerboy2015/boer-bert-registratiesysteem/pull/1#discussion_r2568391123

--- next.js/src/app/Sidebar/SideBarTab.tsx
+++ next.js/src/app/Sidebar/SideBarTab.tsx
@@ -1,10 +1,17 @@
 "use client";
 import Image from "next/image";
-const test = null;
-// prettier-ignore
-export default function SideBarTab({ icon, name }: { icon: string, name: string }) {
+
+interface SideBarTabProps {
+  icon: string;
+  name: string;
+}
+
+export default function SideBarTab({ icon, name }: SideBarTabProps) {
   return (
-    <button onClick={()=> window.location.replace(`/${name}`)} className="flex mb-5 hover:bg-[#556483] rounded-2xl p-2 items-center">
+    <button
+      onClick={() => window.location.replace(`/${name}`)}
+      className="flex mb-5 hover:bg-[#556483] rounded-2xl p-2 items-center"
+    >
       <Image src={icon} alt="error" className="mr-3 invert w-10" />
       <p className="text-2xl ">{name}</p>
     </button>
