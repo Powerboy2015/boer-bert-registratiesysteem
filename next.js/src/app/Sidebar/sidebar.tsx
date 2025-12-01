5e602240d5b3060a138b531763bd797d1599fcfa
lars <larskonermann@gmail.com>
2025-12-01 10:18:50 +0100

    changes

--- next.js/src/app/Sidebar/sidebar.tsx
+++ next.js/src/app/Sidebar/sidebar.tsx
@@ -1,15 +1,6 @@
 "use client";
 
-import account from "./icons/person.svg";
-import logout from "./icons/box-arrow-left.svg";
-import calender from "./icons/calendar-date.svg";
-import settings from "./icons/gear.svg";
-import clock from "./icons/clock.svg";
-import view from "./icons/view-stacked.svg";
-import SideBarTab from "./SideBarTab";
 import { useState } from "react";
-import Image from "next/image";
-import list from "./icons/list.svg";
 
 export default function SideBar() {
   let name = "Bert Bertson";
@@ -24,9 +15,7 @@ export default function SideBar() {
       <button
         onClick={() => setStatusValue(!statusValue)}
         className="md:hidden fixed z-50 bottom-0 bg-[#556483] rounded-4xl p-2"
-      >
-        <Image src={settings} alt="error" className="w-10" />
-      </button>
+      ></button>
       <div
         className={`bg-[#2E3038] min-h-screen w-full min-w-fit ${status} sm:w-1/1 md:w-1/4 md:block xl:w-1/5 2xl:w-1/10`}
       >
@@ -34,22 +23,13 @@ export default function SideBar() {
           {/*Name of the logged on user */}
           <div className="text-4xl pb-5">{name}</div>
           <div className="flex justify-center">
-            <div className="w-fit">
-              {/* Sidebar tabs*/}
-              <SideBarTab icon={calender} name="Overzicht" />
-              <SideBarTab icon={clock} name="reserveringen" />
-              <SideBarTab icon={account} name="Accounts" />
-            </div>
+            <div className="w-fit">{/* Sidebar tabs*/}</div>
           </div>
         </div>
         {/* Put content at the bottom of the sidebar */}
         <div className=" flex-col content-end h-3/6 text-center pb-5">
           <div className="flex justify-center content-end">
-            <div className="w-fit">
-              {/* Bottom sidebar tabs */}
-              <SideBarTab icon={settings} name="Instellingen" />
-              <SideBarTab icon={logout} name="Logout" />
-            </div>
+            <div className="w-fit">{/* Bottom sidebar tabs */}</div>
           </div>
         </div>
       </div>
