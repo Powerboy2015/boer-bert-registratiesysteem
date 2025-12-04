e0cf4beb1900d42d4bb0810084e9aba6ee6a4b26
Zicohh <zicobacker26@gmail.com>
2025-12-04 09:20:13 +0100

    Revert "Features/edit_reservering (#3)"
    This reverts commit 5cca6ff27f60249f7c2b4a1623da6dfad37200c8.
    
    commit has been set up inproperly

--- next.js/src/app/context/modalEnabled.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-export type modalContextType = {
-    modalState: boolean;
-    setModalState: (v: boolean) => void;
-};
-export const modalEnabledContext = createContext<modalContextType>({
-    modalState: false,
-    setModalState: () => undefined,
-});
