aefdb8fc158d5f7b72ff3abaf24ca345ee78eb3c
Lars Konermann <larskonermann@gmail.com>
2025-12-04 11:06:00 +0100

    POST

--- next.js/src/app/api/reserveringen/route.tsx
+++ next.js/src/app/api/reserveringen/route.tsx
@@ -1,4 +1,7 @@
 import mariadb from "mariadb";
+import { NextRequest, NextResponse } from 'next/server';
+
+
 const pool = mariadb.createPool({
     host: 'localhost',
     user: 'root',
@@ -7,22 +10,61 @@ const pool = mariadb.createPool({
     database: "mydatabase"
 });
 
-export function serializeBigInt<T>(data: T): T {
-    return JSON.parse(
-        JSON.stringify(data, (_, value) =>
-            typeof value === "bigint" ? value.toString() : value
-        )
-    );
-}
 
 export async function GET(): Promise<Response> {
     let conn;
     try {
         conn = await pool.getConnection();
-        const data = await conn.query("SELECT * from reserveringen");
-        return Response.json(serializeBigInt(data))
+        const data = await conn.query("SELECT * FROM reserveringen ORDER BY reserveringDatum DESC limit 14");
+        return Response.json(data)
     } catch (err) {
         throw err;
     }
 }
 
+export async function POST(req: NextRequest) {
+    let conn;
+
+    try {
+        const body = await req.json();
+
+        const {
+            Voornaam,
+            Achternaam,
+            email,
+            telNr,
+            adres,
+            PlaatsNummer,
+            DatumAankomst,
+            DatumVertrek
+        } = body;
+
+        conn = await pool.getConnection();
+
+        const result = await conn.query(
+            `INSERT INTO reserveringen 
+            (Voornaam, Achternaam, email, telNr, adres, PlaatsNummer, DatumAankomst, DatumVertrek, reserveringDatum, reserveringBewerkDatum)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())`,
+            [
+                Voornaam,
+                Achternaam,
+                email,
+                telNr,
+                adres,
+                PlaatsNummer,
+                DatumAankomst,
+                DatumVertrek
+            ]
+        );
+
+        return NextResponse.json(
+            { status: 201 }
+        );
+    } catch (err) {
+        console.log(err)
+        return NextResponse.json(err)
+    }
+    finally {
+        if (conn) conn.release();
+    }
+}
