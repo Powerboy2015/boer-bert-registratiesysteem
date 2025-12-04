62caa6b171969ff11aabbc4c3bef0fc4e0f02a46
Zicohh <zicobacker26@gmail.com>
2025-12-04 11:17:05 +0100

    [Refactor] fonts & comments - added fonts to the modal in order to better adhere to desing - added comments to possibly hard to understand code to improve clarity.

--- next.js/src/app/ui/DeleteReservationModal.tsx
+++ next.js/src/app/ui/DeleteReservationModal.tsx
@@ -3,11 +3,21 @@ import { useState } from "react";
 import { createPortal } from "react-dom";
 import { Reservering } from "../reserveringen/Widgets/Reserveringen";
 import ReservationButtonComponent from "./ReservationButtonComponent";
+import { Roboto, Roboto_Mono } from "next/font/google";
 
 interface DeleteReservationProps {
-    reservering: Reservering;
-    DeleteCallback: () => void;
+    reservering: Reservering; // The reservation object that we use to add the info of which reservation you are deleting.
+    DeleteCallback: () => void; // callbacks are functions that we give as parameter and will be used after a certain event e.g. a button click
 }
+
+const roboto = Roboto({
+    weight: "500",
+    subsets: ["latin"],
+});
+const roboto_mono = Roboto_Mono({
+    weight: "400",
+    subsets: ["latin"],
+});
 
 export default function DeleteReservationModal({
     reservering,
@@ -32,6 +42,9 @@ export default function DeleteReservationModal({
                 X
             </button>
             {showModal &&
+                //portals are used to teleport certain styling upstream, here we teleport this modal to the div with id "Modal-Catcher
+                // " otherwise we send it to the body.
+                // https://react.dev/reference/react-dom/createPortal#rendering-a-modal-dialog-with-a-portal
                 createPortal(
                     <div
                         className="absolute w-full h-full top-0 left-0 p-[254px] bg-(--overlay-color) z-10"
@@ -42,13 +55,13 @@ export default function DeleteReservationModal({
                             className="w-full h-full bg-(--color-accent-2) flex flex-col px-8 py-16 gap-12"
                         >
                             <h2
-                                className={`m-0 text-(--color-text) text-[24px]`}
+                                className={`m-0 text-(--color-text) text-[24px] ${roboto.className}`}
                             >
                                 Weet U zeker dat U deze reservering wilt
                                 verwijderen?
                             </h2>
                             <div
-                                className={`flex flex-col gap-2 text-(--color-text) text-[24px]`}
+                                className={`flex flex-col gap-2 text-(--color-text) text-[24px] ${roboto_mono.className}`}
                             >
                                 <div className="flex">
                                     <strong>Naam: </strong>
