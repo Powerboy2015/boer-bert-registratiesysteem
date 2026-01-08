4597f4fe94c14252d8af3030cee812c63743324e
Zicohh <zicobacker26@gmail.com>
2026-01-08 09:23:18 +0100

    added pointer to sidebar and closing the overlay

--- next.js/src/app/reserveringen/NieuweReservering/ReserveringOverlay.tsx
+++ next.js/src/app/reserveringen/NieuweReservering/ReserveringOverlay.tsx
@@ -85,7 +85,7 @@ export default function ReserveringOverlay({
             <div className="fixed flex h-full w-full left-0 justify-center items-center bg-gray-500/90">
                 <div className="bg-[#2E3038] h-1/2 w-1/2 grid relative min-w-fit min-h-fit p-10">
                     <button
-                        className="absolute top-4 right-4"
+                        className="absolute top-4 right-4 cursor-pointer"
                         onClick={() => toggle()}
                     >
                         X
