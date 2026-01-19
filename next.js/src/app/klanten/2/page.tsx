b50ffd26205ee76622c023ca52e57902e817d427
Lars Konermann <144104477+larskonermann@users.noreply.github.com>
2026-01-19 12:10:03 +0100

    totaal prijs (#45)

--- next.js/src/app/klanten/2/page.tsx
+++ next.js/src/app/klanten/2/page.tsx
@@ -69,6 +69,19 @@ export default function Reservering2() {
     fetchAPI();
   }, []);
 
+  let date1 = new Date(DatumAankomst);
+  let date2 = new Date(DatumVertrek);
+
+  // Convert dates to UTC timestamps
+  let utc1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
+  let utc2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
+
+  // Calculate the time difference in milliseconds
+  let timeDiff = Math.abs(utc2 - utc1);
+
+  // Convert milliseconds to days
+  let daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+
   return (
     <>
       <div className="min-h-screen w-full text-[#2c2c2c] font-sans">
@@ -295,7 +308,8 @@ export default function Reservering2() {
                             Totaal
                           </p>
                           <p className="text-[18px] text-right font-bold mt-5 ml-auto">
-                            € --
+                            €
+                            {Plaats === "Groot" ? 30 * daysDiff : 20 * daysDiff}
                           </p>
                         </div>
 
